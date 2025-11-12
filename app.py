cec56b6caee82eaef3ff41326eb922ccf576a2bb
Ryan <38592470+asperand@users.noreply.github.com>
2025-11-12 15:47:29 -0500

    Changed structure, and added threaded workers to complete the loading.
    However, it broke some stuff

--- app.py
+++ app.py
@@ -1,24 +1,51 @@
-from textual import events, on
-from textual.app import App, ComposeResult, RenderResult
-from textual.widgets import Footer, Header, Label, Input, LoadingIndicator
+from textual.app import App, ComposeResult
+from textual.widgets import Footer, Header, Input, LoadingIndicator, Label
+from textual.screen import Screen
+from textual.message import Message
 import roster
+from pathlib import Path
+import time
 
-class RosterGet(App):
+TITLE = "rosterget"
+VERSION = "v0.1"
+
+class RosterApp(App):
+    CSS_PATH = "roster.tcss"
+
     def compose(self) -> ComposeResult:
         yield Header()
-        yield Footer()
-        yield LoadingIndicator()
-        yield Input(placeholder="enter path to excel file here...")
+        yield Input(placeholder="Enter path to .xlsx file...")
     
     def on_mount(self) -> None:
-        self.title = "rosterget"
-        self.sub_title = "v0.1"
+        self.title = TITLE
+        self.sub_title = VERSION
     
-    @on(Input.Submitted)
-    def load_excel(self) -> roster.RosterTable:
-        self.loaded_table = roster.RosterTable(Input.value)
-        self.title = "Successfully loaded " + str(self.loaded_table.get_row_count()) + " rows"
+    async def on_input_submitted(self, message:Input.Submitted) -> None:
+        input_box = self.query_one(Input)
+        self.run_worker(self.load_excel(input_box.value),exclusive=True,thread=True)
+        # Spawned our worker thread. Switch the UI over to our loading screen.
+        self.push_screen(LoadingScreen())
+        # This loading screen works while the background work is going.
+
+    async def load_excel(self,path:str):
+        self.loaded_table = roster.RosterTable(Path(path))
+        self.title = "Loaded " + str(self.loaded_table.get_row_count()) + " Rows"
+        self.sub_title = TITLE + " " + VERSION
+        # Since work is done, I'd like to switch to the next screen.
+        # I've tried screen_push(), which causes the app to close.
+        # I've tried screen_switch(), which causses the app to freeze.
+        # Would I have to do this in the above async function?
+        
+class LoadingScreen(Screen):
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield LoadingIndicator()
+
+class QueryScreen(Screen):
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Label("Finished!")
 
 if __name__ == "__main__":
-    app = RosterGet()
+    app = RosterApp()
     app.run()
