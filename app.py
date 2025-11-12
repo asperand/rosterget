733cf9b5849107bdb7515b229d9cf9596e4f2305
Ryan <38592470+asperand@users.noreply.github.com>
2025-11-12 13:03:16 -0500

    Added input, unsure if works

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 from textual.app import App, ComposeResult, RenderResult
-from textual.widgets import Footer, Header, Label, Input
+from textual.widgets import Footer, Header, Label, Input, LoadingIndicator
+import roster
 
 class RosterGet(App):
-    BINDINGS = [("q", "newquery", "New Query")]
-
     def compose(self) -> ComposeResult:
         yield Header()
         yield Footer()
+        yield LoadingIndicator()
+        yield Input(placeholder="enter path to excel file here...")
     
     def on_mount(self) -> None:
         self.title = "rosterget"
         self.sub_title = "v0.1"
+    
+    @on(Input.Submitted)
+    def load_excel(self) -> roster.RosterTable:
+        self.loaded_table = roster.RosterTable(Input.value)
+        print("Loaded table successfully.")
 
 if __name__ == "__main__":
     app = RosterGet()
